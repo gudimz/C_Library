00ebae5a84b2679af0f89cb075ab5f236a9e4dfb
GuDimz <zgudimz@gmail.com>
2020-11-06 00:40:36 +0300

    itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,72 +6,54 @@
 /*   By: agigi <agigi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:58:30 by agigi             #+#    #+#             */
-/*   Updated: 2020/11/04 22:57:01 by agigi            ###   ########.fr       */
+/*   Updated: 2020/11/06 00:37:29 by agigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int ft_lenstr(long num)
+{
+	int i;
+	
+	i = 0;
+	if (num < 9)
+		num *= -1;
+	while (num > 0)
+	{
+		num = num / 10;
+		i++;
+	}
+	return (i);
+}
+
+static char *ft_nbr_itoa(int num)
+{
+	char *str;
+	long nb;
+	int size;
+	
+	nb = num;
+	size = ft_lenstr(nb);
+	if (!(str = (char *)malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	str[size + 1] = '\0';
+	if (nb < 0)
+		nb *= -1;
+	while (size--)
+	{
+		str[size] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	return (str);
+}
+
 char *ft_itoa(int n)
 {
 	char *res;
-	char tmp;
-	int size;
-	int j;
-	int i;
-
-	size = 0;
-	j = n;
-	if (n < 0)
-	{
-		n *= -1;
-		size++;
-	}
 	
-	while (n > 9)
-	{
-		n = n / 10;
-		size++;
-	}
-	if (!(res = (char *)malloc(size + 2)))
-		return (NULL);
-	i = 0;
-	n = j;
+	res = ft_nbr_itoa(n);
 	if (n < 0)
-	{
-		n = n * -1;
-		// res[size] = '-';
-		// size--;
-	}
-	while (size--)
-	{
-		res[i] = (n % 10) + '0';
-		n = n / 10;
-		i++;
-	}
-	res[i++] = (n % 10) + '0';
-	res[i] = '\0';
-	j = 0;
-	while (j < i / 2)
-	{
-		tmp = res[j];
-		res[j] = res[i - 1 - j];
-		res[i - 1 - j] = tmp;
-		j++;
-	}
-	//создаешь временную строку и заполняешь ее через пут_нбр при этом передаешь положительно ечисло в лонге 
-	//если число отр то стр джоин с минусом иначпе с пустой строкой при этом память выделяется в джойне 
-	printf("%s", res);
-	res[j] = '\0';
-	if (n < 0)
-		ft_strjoin("-", res);
+		res = (ft_strjoin("-", res));
 	return (res);
-}
-
-
-int main(int argc, char const *argv[])
-{
-	
-	printf("%s\n", ft_itoa(-12345));
-	return 0;
 }

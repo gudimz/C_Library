b50a9ea654b8de33c322fbee71c8d83946b053ba
GuDimz <zgudimz@gmail.com>
2020-12-07 22:46:35 +0300

    substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,11 +6,18 @@
 /*   By: agigi <agigi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 21:33:50 by agigi             #+#    #+#             */
-/*   Updated: 2020/11/12 23:07:13 by agigi            ###   ########.fr       */
+/*   Updated: 2020/12/07 22:45:14 by agigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static	size_t ft_min(size_t num1, size_t num2)
+{
+	if (num1 > num2)
+		return (num2);
+	return (num1);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -24,6 +31,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str = (char *)s;
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
+	len = ft_min(ft_strlen(s) - (size_t)start, len);
 	if (!(sub = (char *)malloc(len + 1)))
 		return (NULL);
 	while (i < len)
